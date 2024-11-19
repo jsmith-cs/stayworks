faabbfdd2b3a89dcbcbea577b8fc8718ceeb91fa
rdinh97 <dinhr2@mcmaster.ca>
2024-11-18 20:54:10 -0500

    ChartJs Overview Mock

--- src/app/landing-page/login-page/dashboard/overview/overview.component.ts
+++ src/app/landing-page/login-page/dashboard/overview/overview.component.ts
@@ -14,6 +14,12 @@ import { SidebarModule } from 'primeng/sidebar';
 import { SplitterModule } from 'primeng/splitter';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { Chart,registerables} from 'chart.js';
+import { PanelModule } from 'primeng/panel';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+Chart.register(...registerables);
 @Component({
   selector: 'app-profile',
   standalone: true,
@@ -28,60 +34,103 @@ import { ReactiveFormsModule } from '@angular/forms';
     CardModule,
     RouterLink,
     RouterLinkActive,
-    ReactiveFormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.css',
 })
 export class OverviewComponent implements OnInit {
   overview_card: any[] = [];
+  // data: any[] = [];
+  labels = ["1","2","3","4","5","6","7","8","9","10","11","12"];
+  public data = {
+    labels: this.labels,
+    datasets: [
+      {
+        label: 'Revenue',
+        data: [17000,18000,19000,20000,22000,22000,22000,19000,20000,22000,22000,22000],
+        borderColor: '#36A2EB',
+        backgroundColor: '#36A2EB',
+      },
+      {
+        label: 'Expense',
+        data: [1000,2000,3000,1500,2500,4000,1563,3000,1500,2500,4000,1563],
+        borderColor: '#FF6384',
+        backgroundColor:'#FF6384',
+      }
+    ],
+    
+  };
+  public config:any = {
+    type: 'bar',
+    data: this.data,
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top',
+        },
+        title: {
+          display: true,
+          text: 'Revenue and Expense'
+        }
+      },scales: {
+        y: {
+          title: {
+            display: true,
+            align: 'center',
+            text: 'Dollars $'
+          
+          },
+        },
+        x: {
+          title: {
+            display: true,
+            align: 'center',
+            text: 'Months $'
+          
+          },
+        },
+      },
+      
+    },
+  };
 
+  chart: any;
   constructor(private primengConfig: PrimeNGConfig) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.primengConfig.ripple = true;
 
     this.overview_card = [
       {
-        label: 'Current MRR',
-        content: 'Lorem ipsum dolor sit ',
+        label: 'Properties',
+        content: '5',
         class: 'documents-card',
       },
       {
-        label: 'Current Customers',
-        content: 'Lorem ipsum dolor sit ',
+        label: 'Tenants',
+        content: '20',
         class: 'properties-card',
       },
       {
-        label: 'Active Customers',
-        content: 'Lorem ipsum dolor sit ',
+        label: 'Expected Revenue This Month',
+        content: '$22,000.00',
         class: 'payments-card',
       },
       {
-        label: 'Churn Rate',
-        content: 'Lorem ipsum dolor sit ',
+        label: 'Expenses This Month',
+        content: '$1,563.00',
         class: 'payment-due-card',
       },
-      {
-        label: 'Trend',
-        content: 'Lorem ipsum dolor sit ',
-        class: 'trend-card',
-      },
-      {
-        label: 'Support Tickets',
-        content: 'Lorem ipsum dolor sit ',
-        class: 'support-tickets-card',
-      },
-      {
-        label: 'Sales',
-        content: 'Lorem ipsum dolor sit ',
-        class: 'sales-card',
-      },
-      {
-        label: 'Customer demographics',
-        content: 'Lorem ipsum dolor sit ',
-        class: 'demographics-card',
-      },
+     
     ];
+
+    this.chart = new Chart ('MyChart',this.config);
+     
   }
+
+  
+
+
 }
